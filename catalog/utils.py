e79a6a9b96b1a4b1c7d58e23fb8686759eb48585
Lekant-chek <lekmors@yandex.ru>
2022-05-12 22:01:58 +0500

    pagination, forms

---
+++ catalog/utils.py
@@ -0,0 +1,25 @@
+from django.db.models import Count
+
+from .models import *
+
+menu = [{'title': "Грамматика", 'url_name': "grammar"},
+        {'title': "Добавить учебные материалы", 'url_name': "addpoint"}
+        ]
+
+
+class DataMixin:
+    paginate_by = 2
+
+    def get_user_context(self, **kwargs):
+        context = kwargs
+        category = Category.objects.annotate(Count('point'))
+
+        user_menu = menu.copy()
+        if not self.request.user.is_authenticated:
+            user_menu.pop(2)
+
+        context['menu'] = user_menu
+        context['category'] = category
+        if 'category_selected' not in context:
+            context['category_selected'] = 0
+        return context
